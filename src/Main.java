4c005c3f7c292c098dccddb43234bc274c840abd
Rammah <nydoerd@gmail.com>
2021-03-18 15:05:27 -0700

    Add parseMap method to UI class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import UI.PlayerSelection;
+import UI.UI;
+
+import java.io.FileNotFoundException;
 
 public class Main {
-    public static void main(String[] args) {
-        // Don't forget to change the project working directory ../The-Smurs
-        PlayerSelection playerSelection = new PlayerSelection();
-        playerSelection.setVisible(true);
+    public static void main(String[] args) throws FileNotFoundException {
+        // Don't forget to change the project working directory ../The-Smurfs
+        new PlayerSelection().setVisible(true);
+       //new UI();
     }
 }
 
--- src/UI/UI.java
+++ src/UI/UI.java
@@ -1,4 +1,37 @@
 package UI;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class UI {
+
+    public UI() throws FileNotFoundException {
+        //Testing parseMap
+        parseMap("src/harita.txt");
+    }
+
+    private ArrayList<String> parseMap(String mapPath) throws FileNotFoundException {
+        ArrayList<String> enemyData= new ArrayList<>();
+        File map = new File(mapPath);
+        Scanner mapScanner = new Scanner(map);
+        while (mapScanner.hasNextLine()){
+            //Karakter:Gargamel,Kapi:A
+            String currentLine = mapScanner.nextLine();
+            if(currentLine.contains("Karakter") || currentLine.contains("karakter")){
+                String doorToken[] =currentLine.split(",Kapi:");
+                String characterToken[] = doorToken[0].split(":");
+                String enemyCharacter=  characterToken[1];
+                String enemyDoor = doorToken[1];
+                enemyData.add(enemyCharacter);
+                enemyData.add(enemyDoor);
+            }
+
+        }
+
+        //testing parsing output :
+        System.out.print(enemyData);
+        return  enemyData;
+    }
 }
